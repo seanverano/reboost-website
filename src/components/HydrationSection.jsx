956d2706a9612b707d2b19467bd19c75e3fe9f12
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-11-07 20:18:34 +0800

    Add lists of subfeatures in feature sections

--- src/components/HydrationSection.jsx
+++ src/components/HydrationSection.jsx
@@ -1,22 +1,48 @@
 import hydration from "../assets/images/hydration.png";
 import hydration_two from "../assets/images/hydration_two.png";
+import { FaCircleCheck } from "react-icons/fa6";
 
 const HydrationSection = () => {
   return (
     <>
-      <section className="flex w-full h-[150vh]">
-        <div className="font-manrope w-[50%] bg-[#E8F8EE] relative flex flex-col items-center">
+      <section className="flex w-full h-[125vh]">
+        <div className="font-manrope w-[50%] bg-[#E8F8EE] relative flex flex-col items-center border-r-2 border-b-2 border-[#019963] border-solid">
           <div className="flex flex-col justify-center h-full">
-            <p className="font-manrope text-[#000000] text-lg ml-12 px-5 font-normal">
+            <p className="font-manrope text-[#019963] text-4xl ml-12 px-5 font-semibold">
               Hydration Tracker
             </p>
             <h1 className="font-playfair text-[#000000] text-7xl font-semibold ml-12 px-5 leading-snug mt-12">
               Hydrate and Own the Day
             </h1>
-            <p className="font-manrope text-[#000000] text-lg ml-12 px-5 font-normal">
+            <p className="font-manrope text-[#000000] text-xl ml-12 px-5 font-normal mt-8 mb-4">
               Track and log daily water intake for better health.
             </p>
+            <ul className="font-manrope text-[#000000] text-lg ml-12 px-5 font-normal space-y-6 mt-8">
+              <li className="flex items-center">
+                <FaCircleCheck className="text-[#019963] mr-4" />
+                Select your beverage type and set the volume amount
+              </li>
+              <li className="flex items-center">
+                <FaCircleCheck className="text-[#019963] mr-4" />
+                Create your own custom beverage and volume
+              </li>
+              <li className="flex items-center">
+                <FaCircleCheck className="text-[#019963] mr-4" />
+                Log your drink intake and track your progress
+              </li>
+              <li className="flex items-center">
+                <FaCircleCheck className="text-[#019963] mr-4" />
+                Adjust your daily volume goal anytime
+              </li>
+              <li className="flex items-center">
+                <FaCircleCheck className="text-[#019963] mr-4" />
+                Reset and check your daily hydration logs
+              </li>
+            </ul>
           </div>
+
+          <div className="absolute top-1/2 right-0 transform -translate-y-1/2 w-0 h-0 border-t-8 border-r-8 border-b-8 border-[#EEF8FB] border-l-[#EEF8FB]"></div>
+          <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-b-8 border-r-8 border-[#EEF8FB] border-t-[#EEF8FB]"></div>
         </div>
 
         <div className="w-[50%] bg-[#ffffff] flex flex-col justify-center items-center">
@@ -25,7 +51,7 @@ const HydrationSection = () => {
             alt="Reboost Logo"
             className="w-[256px] h-[256px]"
           />
-          <div className="border-t-8 border-solid border-[#E8F8EE] my-4 w-full"></div>
+          <div className="border-t-2 border-solid border-[#019963] my-4 w-full"></div>
           <img
             src={hydration_two}
             alt="Reboost Logo"
--- src/components/WellnessSection.jsx
+++ src/components/WellnessSection.jsx
@@ -1,17 +1,18 @@
 import hydration from "../assets/images/hydration.png";
 import hydration_two from "../assets/images/hydration_two.png";
+import { FaCircleCheck } from "react-icons/fa6";
 
 const WellnessSection = () => {
   return (
     <>
-      <section className="flex w-full h-[150vh]">
+      <section className="flex w-full h-[125vh]">
         <div className="w-[50%] bg-[#ffffff] flex flex-col justify-center items-center">
           <img
             src={hydration}
             alt="Reboost Logo"
             className="w-[256px] h-[256px]"
           />
-          <div className="border-t-8 border-solid border-[#EEF8FB] my-4 w-full"></div>
+          <div className="border-t-2 border-solid border-[#1CABE3] my-4 w-full"></div>
           <img
             src={hydration_two}
             alt="Reboost Logo"
@@ -19,18 +20,44 @@ const WellnessSection = () => {
           />
         </div>
 
-        <div className="font-manrope w-[50%] bg-[#EEF8FB] relative flex flex-col items-center">
+        <div className="font-manrope w-[50%] bg-[#EEF8FB] relative flex flex-col items-center border-t-2 border-l-2 border-[#1CABE3] border-solid">
           <div className="flex flex-col justify-center h-full">
-            <p className="font-manrope text-[#000000] text-lg ml-12 px-5 font-normal">
+            <p className="font-manrope text-[#1CABE3] text-transparent bg-clip-text text-4xl ml-12 px-5 font-semibold">
               Wellness Reminder
             </p>
             <h1 className="font-playfair text-[#000000] text-7xl font-semibold ml-12 px-5 leading-snug mt-12">
               Improve Wellness with a Break
             </h1>
-            <p className="font-manrope text-[#000000] text-lg ml-12 px-5 font-normal">
+            <p className="font-manrope text-[#000000] text-xl ml-12 px-5 font-normal mt-8 mb-4">
               Get break reminders to recharge and stay productive.
             </p>
+            <ul className="font-manrope text-[#000000] text-lg ml-12 px-5 font-normal space-y-6 mt-8">
+              <li className="flex items-center">
+                <FaCircleCheck className="text-[#1CABE3] mr-4" />
+                Input reminder title (e.g., Eye Break, Hydration)
+              </li>
+              <li className="flex items-center">
+                <FaCircleCheck className="text-[#1CABE3] mr-4" />
+                Add custom details or notes
+              </li>
+              <li className="flex items-center">
+                <FaCircleCheck className="text-[#1CABE3] mr-4" />
+                Set a timer for your reminder
+              </li>
+              <li className="flex items-center">
+                <FaCircleCheck className="text-[#1CABE3] mr-4" />
+                Track time remaining until your next reminder notification
+              </li>
+              <li className="flex items-center">
+                <FaCircleCheck className="text-[#1CABE3] mr-4" />
+                Receive a notification when the timer ends, even when the
+                extension is closed
+              </li>
+            </ul>
           </div>
+
+          <div className="absolute top-1/2 left-0 transform -translate-y-1/2 w-0 h-0 border-t-8 border-l-2 border-b-2 border-[#E8F8EE] border-r-[#E8F8EE]"></div>
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-0 h-0 border-t-8 border-l-2 border-r-2 border-[#E8F8EE] border-b-[#E8F8EE]"></div>
         </div>
       </section>
     </>
