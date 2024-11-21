544d0138d961b057b6d1e896430fdbfd96f37f2f
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-11-21 13:07:44 +0800

    Add vercel json and scroll to top component for clean user navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
 import ResourcesPage from "./pages/ResourcesPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
