cf4761f1e1921ec70a947ece858969d377a1ce8a
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-11-22 15:30:42 +0800

    Update ScrollToTop component

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,14 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { useLocation } from "react-router-dom";
 
-function ScrollToTop() {
-  const { pathname } = useLocation();
+const ScrollToTop = () => {
+  const { pathname, hash } = useLocation();
+
+  const scrollToSection = useCallback(() => {
+    if (hash) {
+      const element = document.getElementById(hash.slice(1));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [hash]);
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    scrollToSection();
+  }, [pathname, hash, scrollToSection]);
 
   return null;
-}
+};
 
 export default ScrollToTop;
