13fdb569318fffe4a6a5c8df007defa8ef470a9a
Sean Dustin Verano <verano.seandustin@gmail.com>
2024-11-22 15:26:37 +0800

    import navigate from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
 import ResourcesPage from "./pages/ResourcesPage";
